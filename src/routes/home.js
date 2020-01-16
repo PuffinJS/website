24ef292338e7955410e865516e6617e2b98aa5e3
Marc Espín <mespinsanz@gmail.com>
2020-01-16 14:07:42 +0100

    Little test

--- src/routes/home.js
+++ src/routes/home.js
@@ -11,12 +11,18 @@ const Home = puffin.element(`
         <centeredAlign>
             <h3>npm install @mkenzo_8/puffin </h3>
         </centeredAlign>
+        <button click="$test">.</button>
     </div>
 `,{
     components:{
         centeredAlign,
         Logo
-    }
+    },
+    methods:[
+        function test(){
+            console.log('.')
+        }
+    ]
 })
 
 export default Home
