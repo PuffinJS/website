d5fdedb227ee267773dfd62ba5e491e64486ec98
Marc Espín <mespinsanz@gmail.com>
2020-01-19 00:01:31 +0100

    Fixed Github Organization link

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -4,7 +4,7 @@ const Contact = puffin.element(`
     <div>
         <h3>Contact</h3>
         <p>eMail: mespinsanz@gmail.com </p>
-        <p><a href="https://google.com" style="padding:0;text-decoration:underline;">Github Organization</a></p>
+        <p><a href="https://github.com/PuffinJS" style="padding:0;text-decoration:underline;">Github Organization</a></p>
     </div>
 `)
 
