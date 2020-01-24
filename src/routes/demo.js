a0e85c0039980a65c491cc5ca9b7ed3f847b282c
Marc Espín <mespinsanz@gmail.com>
2020-01-24 22:41:58 +0100

    Added twitter link

--- src/routes/demo.js
+++ src/routes/demo.js
@@ -2,13 +2,14 @@ import { puffin } from '@mkenzo_8/puffin'
 
 const Demo = puffin.element(`
     <div>
+        <h2>Simple Demo</h2>
         <iframe
-        src="https://codesandbox.io/embed/puffinjs-demo1-xb2q9"
-        style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
-        title="puffinjs-demo1"
-        allow="geolocation; microphone; camera; midi; vr; accelerometer; gyroscope; payment; ambient-light-sensor; encrypted-media; usb"
-        sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
-    ></iframe>
+            src="https://codesandbox.io/embed/puffinjs-demo1-xb2q9"
+            style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
+            title="puffinjs-demo1"
+            allow="geolocation; microphone; camera; midi; vr; accelerometer; gyroscope; payment; ambient-light-sensor; encrypted-media; usb"
+            sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin">
+        </iframe>
     </div>
 `)
 
